7149e9cb52fa950730ae3d3788bf62d16b78f20f
apierce1992 <apierce1992@gmail.com>
2015-09-22 16:26:48 -0400

    more tests going

--- textminer/validator.py
+++ textminer/validator.py
@@ -12,5 +12,14 @@ def binary_even(x):
 def hex(x):
     return re.match(r'[A-Fa-f0-9]{3}', x)
 
+
 def word(x):
     return re.match(r'[A-Za-z0-9-]{4}', x)
+
+
+def words(x, count=None):
+    return word(x)
+
+
+def phone_number(x):
+    return re.match(r'\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}')
